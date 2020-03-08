d25bb34628282a3a979a2bffab438ee16ce4f1a4
paszku <paszku@icloud.com>
2020-03-08 11:06:15 +0100

    Task 2

--- src/js/index.js
+++ src/js/index.js
@@ -1,28 +1,39 @@
 /* Here /* Here goes your JS code */
              /* Here goes your JS code */
 const popupHide = document.querySelector(".popup");
-// popupHide.style.display = "none";
+popupHide.style.display = "none";
 
-const btnMain = document.getElementById("clickMeBtn"); 
+const btnMain = document.querySelector(".btn-main");
 const checkbox = document.querySelector(".checkbox");  
-const submit = document.querySelector(".submit--button");  
+const submit = document.getElementById("sign-in-submit");  
 const eMail = document.querySelector(".email");
 const password = document.querySelector(".password");
 const successMessage = document.querySelector(".success--message");
-successMessage.style.display = "none";
+const cancel = document.querySelector(".close");
+// successMessage.style.display = "none";
 
-btnMain.addEventListener("click", () => { 
-    popupHide.style.display = "block";
+btnMain.addEventListener('click', function(){
+    popupHide.style.display = "flex";
+    btnMain.style.display = "none";
+
 })
 
-checkbox.addEventListener('click', () => {
-    submit.classList.toggle('checkbox--checked');
+cancel.addEventListener('click', function(){
+    popupHide.style.display = "none";
+    btnMain.style.display = "block";
+
 })
+
+
+
+// checkbox.addEventListener('click', () => {
+//     submit.classList.toggle('checkbox--checked');
+// })
 
 submit.addEventListener('click', () => { 
     setTimeout( () => {
         popupHide.style.display = "none";
-        successMessage.style.display = "block";
+        // successMessage.style.display = "block";
         btnMain.style.display = "none";
         }, 3000);
 
